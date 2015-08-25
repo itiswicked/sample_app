bb789e3b3abbfe1ab2da02e1adc12d010ad683d1
Nathan <nathanrbourke@gmail.com>
2015-08-25 13:05:26 -0400

    Create redirect to user page upon successful sign up, create flash upon successful signup, create integration tests for valid and invalid singup form submission.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,29 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render(:new)
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  #####################
+  ## PRIVATE METHODS ##
+  #####################
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
   end
 
 end
